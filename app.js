f73c46a52b9af5fc9a592cd9e2ae1de919b8c643
Andres Campuzano <campuxano88@gmail.com>
2020-02-18 08:07:19 -0500

    Now randomly user is printed in HTML.

--- app.js
+++ app.js
@@ -12,3 +12,18 @@ function showPressMeAgain() {
   const pressMeAgain = document.getElementById('pressMeAgain');
   pressMeAgain.classList.remove('hide');
 }
+
+
+fetch("https://randomuser.me/api/")
+  .then(function (response) {
+    // console.log(response);
+    return response.json();
+  })
+  .then(function (user) {
+    console.log("user", user.results[0].name.first, "is generated randomly by API.");
+    document.getElementById("randomUser").innerHTML = user.results[0].name.first;
+    
+  })
+  .catch(function () {
+    console.log("algo falló");
+  });
