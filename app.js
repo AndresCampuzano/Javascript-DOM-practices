79e48c3e247f3824560faf9368ce593cbd4deffa
Andres Campuzano <campuxano88@gmail.com>
2020-02-19 01:01:56 -0500

    for-while-API.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-
+// Changing colors
 function myFunction() {
   const div = document.getElementById("myDIV");
   const h1 = document.getElementById('h1');
@@ -13,17 +13,62 @@ function showPressMeAgain() {
   pressMeAgain.classList.remove('hide');
 }
 
+// API
 
-fetch("https://randomuser.me/api/")
-  .then(function (response) {
-    // console.log(response);
-    return response.json();
-  })
-  .then(function (user) {
-    console.log("user", user.results[0].name.first, "is generated randomly by API.");
-    document.getElementById("randomUser").innerHTML = user.results[0].name.first;
+for (let x = 1; x <= 3; x++) {
+  fetch("https://randomuser.me/api/")
+    .then(function (response) {
+      // console.log(response);
+      return response.json();
+    })
+    .then(function (user) {
+      document.getElementById("randomUser").innerHTML = user.results[0].name.first;
+        console.log("user", user.results[0].name.first, "is generated randomly by API.");      
+    })
+    .catch(function () {
+      console.log("algo falló");
+    });
+}
+
+
+
+
+
+  // //getting name from user
+  // function greeting(yourName) {
+  //   var result = 'Hello ' + yourName;
+  //   document.getElementById('someText').innerHTML = result;
+  // };
+  
+  // var name = prompt('What is your name?');
+  // greeting(name);
+
+
+  //while
+
+  // var num = 0;
+
+  // while (num < 100) {
+  //   num += 1;
+  //   console.log(num);
+  // }
+
+
+  // //for 
+
+  // for (let num = 0; num <= 100; num++) {
+  //   console.log(num);
     
-  })
-  .catch(function () {
-    console.log("algo falló");
-  });
+  // }
+
+  let someNumbers = [12, 458, 2, 4, 7, 89, 50, 10, 12, 18, 15];
+  console.log(someNumbers.sort(function(a,b) {return a-b})); // ascensing order
+  console.log(someNumbers.sort(function(a,b) {return b-a})); // descending order
+  
+
+  let emptyArray = new Array();
+  for (let num = 0; num <= 5; num++) {
+    emptyArray.push(num)
+  }
+  console.log(emptyArray);
+  
